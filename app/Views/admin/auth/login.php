80326fd54fba1723562119850da60af630d0ae3c
yasterjoshua14 <yasterjoshua@gmail.com>
2026-04-29 16:40:35 +0800

    dado

--- app/Views/admin/auth/login.php
+++ app/Views/admin/auth/login.php
@@ -1,17 +1,22 @@
-<?= $this->extend('layouts/main') ?>
+<?php
+/**
+ * @var \CodeIgniter\View\View $this
+ */
+?>
+<?php $this->extend('layouts/main'); ?>
 
-<?= $this->section('content') ?>
+<?php $this->section('content'); ?>
     <div class="section-head">
         <div>
             <h2>Admin login</h2>
         </div>
     </div>
 
-    <form action="<?= esc(admin_path('login')) ?>" method="post" class="grid">
+    <form action="<?= view_esc(admin_path('login')) ?>" method="post" class="grid">
         <?= csrf_field() ?>
 
         <div>
-            <input type="email" id="email" name="email" value="<?= esc(old('email')) ?>" placeholder="Email" required>
+            <input type="email" id="email" name="email" value="<?= view_esc(old('email')) ?>" placeholder="Email" required>
         </div>
 
         <div>
@@ -21,5 +26,5 @@
         <button type="submit" class="btn btn-primary">Enter Admin Console</button>
     </form>
 
-    <p class="auth-switch"><a class="link-inline" href="<?= esc(tenant_path('login')) ?>">Use the tenant login</a>.</p>
-<?= $this->endSection() ?>
+    <p class="auth-switch"><a class="link-inline" href="<?= view_esc(tenant_path('login')) ?>">Use the tenant login</a>.</p>
+<?php $this->endSection(); ?>
