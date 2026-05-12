ee1b5e22d64792ac0fec58cb29a108f304e726ec
yasterjoshua14 <yasterjoshua@gmail.com>
2026-05-12 12:34:05 +0800

    modified: layout

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -5,7 +5,7 @@
 ?>
 <footer class="shell-panel footer-panel">
     <div class="footer-panel__body">
-        <strong>Copyright &copy; 2026 <a href="">YJD Solutions</a> All rights reserved. </strong>
+        <strong>Copyright &copy; 2026 <a href="">MW Solutions</a> All rights reserved. </strong>
         <p><b>Version</b> CI4.v1</p>
     </div>
 </footer>
